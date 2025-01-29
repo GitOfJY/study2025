ac088a46877c567d10a62947c97187fb30b9fa49
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2025-01-29 13:35:46 +0900

    Add files via upload

---
+++ java-start/src/variable/ex/VarEx1.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 4;
+        System.out.println(num2+num1);
+        System.out.println(num2-num1);
+        System.out.println(num2*num1);
+    }
+}
