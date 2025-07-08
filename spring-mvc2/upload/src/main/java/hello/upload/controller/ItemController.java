1359dd75386ac01f82f1ed2a6001e3b4c68e2489
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2025-07-08 21:35:04 +0900

    Add files via upload

---
+++ spring-mvc2/upload/src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,22 @@
+package hello.upload.controller;
+
+import hello.upload.domain.ItemRepository;
+import hello.upload.file.FileStore;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem() {
+
+    }
+}
---
+++ spring-mvc2/upload/src/main/java/hello/upload/domain/Item.java
@@ -0,0 +1,11 @@
+package hello.upload.domain;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+}
