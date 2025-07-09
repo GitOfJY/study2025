07d850626a0988b0ffa8d4ec2101d0e28c26ea49
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2025-07-09 21:15:33 +0900

    Add files via upload

--- java-adv1/src/thead/bounded/BoundedMain.java
+++ java-adv1/src/thead/bounded/BoundedMain.java
@@ -2,6 +2,8 @@ package thead.bounded;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
@@ -13,11 +15,15 @@ public class BoundedMain {
         // BoundedQueue queue = new BoundedQueueV2(2);
         // BoundedQueue queue = new BoundedQueueV3(2);
         // BoundedQueue queue = new BoundedQueueV4(2);
-        BoundedQueue queue = new BoundedQueueV5(2);
+        // BoundedQueue queue = new BoundedQueueV5(2);
+        // BoundedQueue queue = new BoundedQueueV6_2(2);
+        // BoundedQueue queue = new BoundedQueueV6_3(2);
+        BoundedQueue queue = new BoundedQueueV6_4(2);
+        // BlockingQueue<String> queue = new ArrayBlockingQueue<>(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택
-        producerFirst(queue); // 생산자 먼저 실행
-        // consumerFirst(queue); // 소비자 먼저 실행
+        // producerFirst(queue); // 생산자 먼저 실행
+        consumerFirst(queue); // 소비자 먼저 실행
     }
 
     private static void producerFirst(BoundedQueue queue) {
