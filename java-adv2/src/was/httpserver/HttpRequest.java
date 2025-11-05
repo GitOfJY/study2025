ab9c7f76d268b704bc61cfc20b18572f2b0925b2
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2025-11-05 11:11:35 +0900

    Add files via upload

--- java-adv2/src/was/httpserver/HttpRequest.java
+++ java-adv2/src/was/httpserver/HttpRequest.java
@@ -7,6 +7,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+import static util.MyLogger.log;
+
 public class HttpRequest {
     private String method;
     private String path;
@@ -16,7 +18,28 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parserHeader(reader);
-        // 메시지 바디는 이후에 처리
+        parseBody(reader);
+    }
+
+    private void parseBody(BufferedReader reader) throws IOException {
+        if (!headers.containsKey("Content-Type")) {
+            return;
+        }
+
+        int contentLength = Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars = new char[contentLength];
+        int read = reader.read(bodyChars);
+        if (read != contentLength) {
+            throw new IOException("Fail to read entire body. Expected " + contentLength + " bytes but got " + read);
+        }
+
+        String body = new String(bodyChars);
+        log("HTTP Message Body: " + body);
+
+        String contentType = headers.get("Content-Type");
+        if ("application/x-www-form-urlencoded".equals(contentType)) {
+            parseQueryParameters(body);
+        }
     }
 
     private void parseRequestLine(BufferedReader reader) throws IOException {
---
+++ java-adv2/src/was/httpserver/servlet/annotation/AnnotationServletV1.java
@@ -0,0 +1,46 @@
+package was.httpserver.servlet.annotation;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+import was.httpserver.HttpServlet;
+import was.httpserver.PageNotFoundException;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class AnnotationServletV1 implements HttpServlet {
+    private final List<Object> controllers;
+
+    public AnnotationServletV1(List<Object> controllers) {
+        this.controllers = controllers;
+    }
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        String path = request.getPath();
+
+        for (Object controller : controllers) {
+            Method[] methods = controller.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Mapping.class)) {
+                    Mapping mapping = method.getAnnotation(Mapping.class);
+                    String value = mapping.value();
+                    if (value.equals(path)) {
+                        invoke(controller, request, response, method);
+                    }
+                }
+            }
+        }
+        throw new PageNotFoundException("request = " + path);
+    }
+
+    private static void invoke(Object controller, HttpRequest request, HttpResponse response, Method method) throws IOException {
+        try {
+            method.invoke(controller, request, response);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
